702fa7796ff68e0dc071839c5727d7933e61f2a9
wasim230496 <112865463+wasim230496@users.noreply.github.com>
2024-09-18 17:10:53 +0530

    Add files via upload

---
+++ toDoList.js
@@ -0,0 +1,71 @@
+function addTask(){
+    let container = document.getElementById("tasks");
+    
+        let input = document.getElementById("userInput").value;
+        // taskText.innerHTML = input;  
+        if (input.trim() === "") {
+            alert("Please enter a valid task.");
+            return;
+            }
+            console.log("useinput is ",input);
+            
+        let taskBox = document.createElement('div');
+            taskBox.className = "tasklist";
+            
+        let taskText = document.createElement('ol');
+        // taskText.innerHTML = input;  
+        taskText.id = "task";// my line
+        
+        let taskTextli = document.createElement('li');
+        taskTextli.innerHTML = input;
+
+        taskText.appendChild(taskTextli);
+
+
+
+    // let task = document.getElementById("task");// my lines
+    // task.innerHTML=input
+
+    let completeButton = document.createElement("button");
+    completeButton.className ="btn"; 
+    completeButton.innerHTML= "Complete"
+
+    let deleteButton = document.createElement("button");
+    deleteButton.className ="btn"; 
+    deleteButton.innerHTML = "Delete"
+
+    taskBox.appendChild(taskText);
+    taskBox.appendChild(completeButton);
+    taskBox.appendChild(deleteButton);
+
+    container.appendChild(taskBox)
+
+    document.getElementById("userInput").value = "";
+
+    completeButton.addEventListener("click",function(){
+        taskText.style.textDecoration = "line-through";
+    })
+
+    deleteButton.addEventListener("click",function(){
+        container.removeChild(taskBox);
+    });
+    
+}
+
+// let taskAddingButton = 
+document.getElementById("submitButton").addEventListener("click",addTask);
+// taskAddingButton.addEventListener("click",addTask());
+        // let input = document.getElementById("userInput").value;
+        // taskText.innerHTML = input;
+        
+        
+        
+        
+        // let task = document.getElementById("task");
+        // task.innerHTML=input;
+        
+
+
+
+
+    // document.getElementById("submitButton").addEventListener("click",addTask());
