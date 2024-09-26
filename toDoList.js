cd3283ba176d8593f315017c3eec2dbb41d5d2eb
wasim230496 <112865463+wasim230496@users.noreply.github.com>
2024-09-26 06:58:33 +0530

    Update toDoList.js

--- toDoList.js
+++ toDoList.js
@@ -1,4 +1,4 @@
-function addTask(){
+ function addTask(){
         let container = document.getElementById("tasks");
     
         let input = document.getElementById("userInput").value;
@@ -69,3 +69,75 @@ document.getElementById("submitButton").addEventListener("click",addTask);
 
 
     // document.getElementById("submitButton").addEventListener("click",addTask());
+    
+// function addTask(){
+//         let container = document.getElementById("tasks");
+    
+//         let input = document.getElementById("userInput").value;
+//         // taskText.innerHTML = input;  
+//         if (input.trim() === "") {
+//             alert("Please enter a valid task.");
+//             return;
+//             }
+//             console.log("useinput is ",input);
+            
+//         let taskBox = document.createElement('div');
+//             taskBox.className = "tasklist";
+            
+//         let taskText = document.createElement('ul');
+//         // taskText.innerHTML = input;  
+//         // taskText.id = "task";// my line
+        
+//         let taskTextli = document.createElement('li');
+//         taskTextli.innerHTML = input;
+//         taskText.appendChild(taskTextli);
+
+        
+        
+        
+//         // let task = document.getElementById("task");// my lines
+//         // task.innerHTML=input
+        
+//         let completeButton = document.createElement("button");
+//         completeButton.className ="btn"; 
+//         completeButton.innerHTML= "Complete"
+
+//         let deleteButton = document.createElement("button");
+//         deleteButton.className ="btn"; 
+//         deleteButton.innerHTML = "Delete"
+
+//     taskBox.appendChild(taskText);
+//     taskText.appendChild(completeButton);
+//     taskText.appendChild(deleteButton);
+
+//     container.appendChild(taskBox);
+
+//     document.getElementById("userInput").value = "";
+
+//     completeButton.addEventListener("click",function(){
+//         taskText.style.textDecoration = "line-through";
+//     })
+
+//     deleteButton.addEventListener("click",function(){
+//         container.removeChild(taskBox);
+//     });
+    
+// }
+
+// // let taskAddingButton = 
+// document.getElementById("submitButton").addEventListener("click",addTask);
+// // taskAddingButton.addEventListener("click",addTask());
+//         // let input = document.getElementById("userInput").value;
+//         // taskText.innerHTML = input;
+        
+        
+        
+        
+//         // let task = document.getElementById("task");
+//         // task.innerHTML=input;
+        
+
+
+
+
+//     // document.getElementById("submitButton").addEventListener("click",addTask());
